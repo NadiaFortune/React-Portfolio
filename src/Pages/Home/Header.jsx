a73c8b092d3808c8cf85931c445ac33041f8aae3
NadiaFortune <hello@nadiafortune.co.uk>
2024-03-05 21:35:51 +0000

    updated index. css mainly for styling of site

--- src/Pages/Home/Header.jsx
+++ src/Pages/Home/Header.jsx
@@ -1,6 +1,25 @@
 export default function HeaderSection () {
     return  (
-        <section>
+        <section id="headerSection" className="headerSection">
+            <div className="headeer--section--content--box">
+                <div  className="header--section--content">
+                    <p className="section--title">Hey, I'm Nadia</p>
+                    <h1 className="header--section--title">
+                        <span className="header--section--title-color">Front End</span>
+                        {" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="header--setion-description">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        <br /> Praesentium adipisci laborum recusandae.
+                        </p>
+            </div>
+            <button className="btn btn-primary">Contact Me</button>
+            </div>
+            <div className="header--section--img w-25 p-3" >
+                <img src="/nadia-7_pp-3.jpg" alt="" />
+            </div>
             
         </section>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './Pages/Home/Header.jsx'
+import './Pages/Home/AboutMe.jsx'
+import './Pages/Home/Contact.jsx'
+import './Pages/Home/Footer.jsx'
+import './Pages/Home/MySkills.jsx'
+import './Pages/Home/NavBar.jsx'
+import './Pages/Home/ProjectGallery.jsx'
+import './Pages/Home/Testimonials.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as bootstrap from 'bootstrap'
 
@@ -10,3 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
